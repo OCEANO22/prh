ed589735c0c27205d298b5a7d0511680ef19a50b
OCEANO22 <n.oceano22@gmail.com>
2025-06-25 19:46:17 +0700

    simpan

--- api/index.ts
+++ api/index.ts
@@ -8,8 +8,16 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api/prhs")) {
-       let { results } = await env.DB.prepare("SELECT * FROM prhs").all();
-      return Response.json(results);
+       if (request.method == 'GET') {
+        let { results } = await env.DB.prepare("SELECT * FROM prhs").all();
+        return Response.json(results);
+      } else if (request.method == 'POST') {
+        const newId = crypto.randomUUID()
+        const input = await request.json<any>()
+        const query = `INSERT INTO prhs(id,nama,email,pesan) values ("${newId}","${input.name}","${input.email}","${input.pesan}")`;
+        const newPrh = await env.DB.exec(query);
+        return Response.json(newPrh);
+      }
     }
 
     return env.ASSETS.fetch(request);
--- src/views/CreatePrhView.vue
+++ src/views/CreatePrhView.vue
@@ -1,8 +1,50 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter();
+
+const name = ref('')
+const email = ref('')
+const pesan = ref('')
+
+const saveData = async () => {
+    const newPrh = JSON.stringify({
+        name: name.value,
+        email: email.value,
+        pesan: pesan.value,
+    })
+
+    const response = await fetch('/api/prhs', {
+        method:'POST',
+        body: newPrh,
+    })
+    const data = await response.json()
+
+    router.push('/')
+}
 </script>
 
 
 <template>
     <h1>Buat PRH</h1>
+
+     <form @submit.prevent="saveData">
+        <div>
+            <label>Nama</label>
+            <input type="text" v-model="name">
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email" v-model="email">
+        </div>
+        <div>
+            <label>Pesan</label>
+            <textarea v-model="pesan"></textarea>
+        </div>
+
+        <div>
+            <input type="submit">
+        </div>
+    </form>
 </template>
